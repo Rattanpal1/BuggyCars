83166195e21f0659fdc281fff266cbc8d68fd052
Rattanpal1 <61667793+Rattanpal1@users.noreply.github.com>
2022-07-19 18:53:57 +1200

    created the skeleton

---
+++ src/main/java/BuggyCars/BuggyCars/App.java
@@ -0,0 +1,13 @@
+package BuggyCars.BuggyCars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
